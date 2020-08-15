9f15e064ff89299ddc5c27fd7e1823d39fb68ade
Nik A Zeyn <anar.zeynalov@hotmail.com>
2020-08-16 03:37:40 +1000

    re-arranged staticfiles to prep for deployment

--- prj_resource_center/prj_resource_center/settings.py
+++ prj_resource_center/prj_resource_center/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR,'static'),
+	os.path.join(BASE_DIR,'staticfiles'),
 ]
 
 # used for collectstatic in prod
-# STATIC_ROOT = os.path.join(BASE_DIR, '/assets/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
